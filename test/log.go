712729b72e97cbed4cc8755972b90cb90a7117d8
Bjørn Sørensen <bso@lunarway.com>
2020-02-27 15:38:44 +0100

    Add a test on idle connections

--- test/log.go
+++ test/log.go
@@ -29,3 +29,45 @@ func (l *logger) Write(p []byte) (int, error) {
 	l.t.Logf("%s", p)
 	return len(p), nil
 }
+
+// RawLogger implements a test logger that will log any info or log lines to a
+// testing.T.Logf function.
+type RawLogger struct {
+	T      *testing.T
+	name   string
+	values []interface{}
+}
+
+var _ logr.Logger = &RawLogger{}
+
+func (t *RawLogger) Info(msg string, keysAndValues ...interface{}) {
+	t.T.Logf("INFO:  %s KEYPAIRS: %v", msg, keysAndValues)
+}
+
+func (t *RawLogger) Enabled() bool {
+	return true
+}
+
+func (t *RawLogger) Error(err error, msg string, keysAndValues ...interface{}) {
+	t.T.Logf("ERROR: %s: %v KEYPAIRS: %v", msg, err, keysAndValues)
+}
+
+func (t *RawLogger) V(level int) logr.InfoLogger {
+	return t
+}
+
+func (t *RawLogger) WithValues(keysAndValues ...interface{}) logr.Logger {
+	return &RawLogger{
+		T:      t.T,
+		name:   t.name,
+		values: append(t.values, keysAndValues...),
+	}
+}
+
+func (t *RawLogger) WithName(name string) logr.Logger {
+	return &RawLogger{
+		T:      t.T,
+		name:   name,
+		values: t.values,
+	}
+}
