lunarway/postgresql-controller

commit 46e333a80e8dd6ea7829ccd53c3d9578ef0c0575

Expire roles when access requests expires (#54)
This PR adds handling of the Start and Stop fields in PostgreSQLUser
resources. It also moves much of the granting logic out of
postgresqluser_controller.go and into a grants/sync.go file where it fits
better with all the database logic.

Overall the main difference is that we now inspect existing roles before
granting the required ones. We then diff the existing ones with the ones we
expected and removes those that are no longer expected. As a safe guard only
roles ending with _read and _readwrite are touched for now. These are the
roles we grant.

Expiration in general is handled in the groupAccesses method of
grants.Granter where Start and Stop are inspected and the specifications
are filtered out if they are not relevant. Combined with the described role
removal above, this leads roles to expire.

Note that we do not have a daemon implementation triggering this reconciliation.
We use operator-sdk's sync interval (default 10 hours) to control how often this
should be done. If we need to be more aggressive with revoking access on
expiration the --resync-period can be set accordingly.

pkg/controller/postgresqluser/postgresqluser_controller.go
@@ -2,10 +2,9 @@ package postgresqluser
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"os"
-	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/go-logr/logr"
@@ -16,7 +15,6 @@ import (
 	"go.lunarway.com/postgresql-controller/pkg/iam"
 	"go.lunarway.com/postgresql-controller/pkg/kube"
 	"go.lunarway.com/postgresql-controller/pkg/postgres"
-	"go.uber.org/multierr"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -66,10 +64,10 @@ func parseFlags(c *ReconcilePostgreSQLUser) {
 	parseError(err, "aws-secret-access-key")
 	hosts, err := FlagSet.GetStringToString("host-credentials-user")
 	parseError(err, "host-credentials-user")
-	c.hostCredentials, err = parseHostCredentials(hosts)
+	c.granter.HostCredentials, err = parseHostCredentials(hosts)
 	parseError(err, "host-credentials: invalid format")
 	var hostNames []string
-	for host := range c.hostCredentials {
+	for host := range c.granter.HostCredentials {
 		hostNames = append(hostNames, host)
 	}
 	c.granter.AllDatabasesReadEnabled, err = FlagSet.GetBool("all-databases-enabled-read")
@@ -122,6 +120,7 @@ func newReconciler(mgr manager.Manager) reconcile.Reconciler {
 	c := &ReconcilePostgreSQLUser{
 		client: mgr.GetClient(),
 		granter: grants.Granter{
+			Now: time.Now,
 			AllDatabases: func(namespace string) ([]lunarwayv1alpha1.PostgreSQLDatabase, error) {
 				return kube.PostgreSQLDatabases(mgr.GetClient(), namespace)
 			},
@@ -171,9 +170,6 @@ type ReconcilePostgreSQLUser struct {
 	awsProfile         string
 	awsAccessKeyID     string
 	awsSecretAccessKey string
-
-	// contains a map of credentials for hosts
-	hostCredentials map[string]postgres.Credentials
 }
 
 // Reconcile reads that state of the cluster for a PostgreSQLUser object and makes changes based on the state read
@@ -217,31 +213,11 @@ func (r *ReconcilePostgreSQLUser) reconcile(reqLogger logr.Logger, request recon
 
 	// User instance created or updated
 	reqLogger = reqLogger.WithValues("user", user.Spec.Name)
-	reqLogger.Info("Updating PostgreSQLUser resource")
+	reqLogger.Info("Reconciling found PostgreSQLUser resource", "user", user.Spec.Name)
 
-	accesses, err := r.granter.GroupAccesses(reqLogger, request.Namespace, user.Spec.Read, user.Spec.Write)
+	err = r.granter.SyncUser(request.Namespace, *user)
 	if err != nil {
-		if len(accesses) == 0 {
-			return reconcile.Result{}, fmt.Errorf("group accesses: %w", err)
-		}
-		reqLogger.Error(err, fmt.Sprintf("Some access requests could not be resolved. Continuating with the resolved ones"))
-	}
-	reqLogger.Info(fmt.Sprintf("Found access requests for %d hosts", len(accesses)))
-
-	hosts, err := r.connectToHosts(accesses)
-	if err != nil {
-		return reconcile.Result{}, fmt.Errorf("connect to hosts: %w", err)
-	}
-	defer func() {
-		err := closeConnectionToHosts(hosts)
-		if err != nil {
-			reqLogger.Error(err, "failed to close connection to hosts")
-		}
-	}()
-
-	err = r.ensurePostgreSQLRoles(reqLogger, user.Spec.Name, accesses, hosts)
-	if err != nil {
-		return reconcile.Result{}, fmt.Errorf("ensure postgresql roles: %w", err)
+		return reconcile.Result{}, fmt.Errorf("sync user grants: %w", err)
 	}
 
 	var awsCredentials *credentials.Credentials
@@ -260,82 +236,4 @@ func (r *ReconcilePostgreSQLUser) reconcile(reqLogger logr.Logger, request recon
 	}
 
 	return reconcile.Result{}, nil
-}
-
-func (r *ReconcilePostgreSQLUser) ensurePostgreSQLRoles(log logr.Logger, name string, accesses grants.HostAccess, hosts map[string]*sql.DB) error {
-	for host, access := range accesses {
-		connection, ok := hosts[host]
-		if !ok {
-			return fmt.Errorf("connection for host %s not found", host)
-		}
-		err := r.ensurePostgreSQLRole(log, connection, name, access)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func databaseSchemas(accesses []grants.ReadWriteAccess) []postgres.DatabaseSchema {
-	var ds []postgres.DatabaseSchema
-	for _, access := range accesses {
-		ds = append(ds, postgres.DatabaseSchema{
-			Name:       access.Database.Name,
-			Schema:     access.Database.Schema,
-			Privileges: access.Database.Privileges,
-		})
-	}
-	return ds
-}
-
-func (r *ReconcilePostgreSQLUser) ensurePostgreSQLRole(log logr.Logger, db *sql.DB, name string, accesses []grants.ReadWriteAccess) error {
-	name = fmt.Sprintf("%s%s", r.rolePrefix, name)
-	log = log.WithValues("operation", "ensurePostgreSQLRole", "name", name)
-	err := postgres.Role(log, db, name, r.grantRoles, databaseSchemas(accesses))
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (r *ReconcilePostgreSQLUser) connectToHosts(accesses grants.HostAccess) (map[string]*sql.DB, error) {
-	hosts := make(map[string]*sql.DB)
-	var errs error
-	for hostDatabase := range accesses {
-		// hostDatabase contains the host name and the database but we expect host
-		// credentials to be without the database part
-		// This will not work for hosts with multiple / characters
-		hostDatabaseParts := strings.Split(hostDatabase, "/")
-		host := hostDatabaseParts[0]
-		database := hostDatabaseParts[1]
-		credentials, ok := r.hostCredentials[host]
-		if !ok {
-			errs = multierr.Append(errs, fmt.Errorf("no credentials for host '%s'", host))
-			continue
-		}
-		connectionString := postgres.ConnectionString{
-			Host:     host,
-			Database: database,
-			User:     credentials.Name,
-			Password: credentials.Password,
-		}
-		db, err := postgres.Connect(log, connectionString)
-		if err != nil {
-			errs = multierr.Append(errs, fmt.Errorf("connect to %s: %w", connectionString, err))
-			continue
-		}
-		hosts[hostDatabase] = db
-	}
-	return hosts, errs
-}
-
-func closeConnectionToHosts(hosts map[string]*sql.DB) error {
-	var errs error
-	for name, conn := range hosts {
-		err := conn.Close()
-		if err != nil {
-			errs = multierr.Append(errs, fmt.Errorf("host %s: %w", name, err))
-		}
-	}
-	return errs
 }

pkg/grants/sync.go
@@ -1,0 +1,118 @@
+package grants
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+
+	lunarwayv1alpha1 "go.lunarway.com/postgresql-controller/pkg/apis/lunarway/v1alpha1"
+	"go.lunarway.com/postgresql-controller/pkg/postgres"
+	"go.uber.org/multierr"
+)
+
+// SyncUser syncronizes a PostgreSQL user's access requests against the roles
+// defined in the host instances. Any excessive roles are removed and missing
+// ones are added.
+func (g *Granter) SyncUser(namespace string, user lunarwayv1alpha1.PostgreSQLUser) error {
+	//   resolve required grants taking expiration into account
+	//   diff against existing
+	//   revoke/grant what is needed
+	accesses, err := g.groupAccesses(namespace, user.Spec.Read, user.Spec.Write)
+	if err != nil {
+		if len(accesses) == 0 {
+			return fmt.Errorf("group accesses: %w", err)
+		}
+		g.Log.Error(err, fmt.Sprintf("Some access requests could not be resolved. Continuating with the resolved ones"))
+	}
+	g.Log.Info(fmt.Sprintf("Found access requests for %d hosts", len(accesses)))
+
+	hosts, err := g.connectToHosts(accesses)
+	if err != nil {
+		return fmt.Errorf("connect to hosts: %w", err)
+	}
+	defer func() {
+		err := closeConnectionToHosts(hosts)
+		if err != nil {
+			g.Log.Error(err, "failed to close connection to hosts")
+		}
+	}()
+
+	err = g.setRolesOnHosts(user.Name, accesses, hosts)
+	if err != nil {
+		return fmt.Errorf("grant access on host: %w", err)
+	}
+
+	return nil
+}
+
+func (g *Granter) connectToHosts(accesses HostAccess) (map[string]*sql.DB, error) {
+	hosts := make(map[string]*sql.DB)
+	var errs error
+	for hostDatabase := range accesses {
+		// hostDatabase contains the host name and the database but we expect host
+		// credentials to be without the database part
+		// This will not work for hosts with multiple / characters
+		hostDatabaseParts := strings.Split(hostDatabase, "/")
+		host := hostDatabaseParts[0]
+		database := hostDatabaseParts[1]
+		credentials, ok := g.HostCredentials[host]
+		if !ok {
+			errs = multierr.Append(errs, fmt.Errorf("no credentials for host '%s'", host))
+			continue
+		}
+		connectionString := postgres.ConnectionString{
+			Host:     host,
+			Database: database,
+			User:     credentials.Name,
+			Password: credentials.Password,
+		}
+		db, err := postgres.Connect(g.Log, connectionString)
+		if err != nil {
+			errs = multierr.Append(errs, fmt.Errorf("connect to %s: %w", connectionString, err))
+			continue
+		}
+		hosts[hostDatabase] = db
+	}
+	return hosts, errs
+}
+
+func closeConnectionToHosts(hosts map[string]*sql.DB) error {
+	var errs error
+	for name, conn := range hosts {
+		err := conn.Close()
+		if err != nil {
+			errs = multierr.Append(errs, fmt.Errorf("host %s: %w", name, err))
+		}
+	}
+	return errs
+}
+
+func (g *Granter) setRolesOnHosts(name string, accesses HostAccess, hosts map[string]*sql.DB) error {
+	var errs error
+	for host, access := range accesses {
+		connection, ok := hosts[host]
+		if !ok {
+			return fmt.Errorf("connection for host %s not found", host)
+		}
+		err := postgres.Role(g.Log, connection, name, g.StaticRoles, databaseSchemas(access))
+		if err != nil {
+			errs = multierr.Append(errs, fmt.Errorf("grant roles: %w", err))
+		}
+	}
+	if errs != nil {
+		return errs
+	}
+	return nil
+}
+
+func databaseSchemas(accesses []ReadWriteAccess) []postgres.DatabaseSchema {
+	var ds []postgres.DatabaseSchema
+	for _, access := range accesses {
+		ds = append(ds, postgres.DatabaseSchema{
+			Name:       access.Database.Name,
+			Schema:     access.Database.Schema,
+			Privileges: access.Database.Privileges,
+		})
+	}
+	return ds
+}

pkg/controller/postgresqluser/postgresqluser_controller_test.go
@@ -2,8 +2,8 @@ package postgresqluser
 
 import (
 	"errors"
-	"fmt"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/go-logr/logr"
@@ -22,90 +22,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 )
-
-func TestReconcile_connectToHosts(t *testing.T) {
-	test.Integration(t)
-	tt := []struct {
-		name            string
-		credentials     map[string]postgres.Credentials
-		hostAccess      grants.HostAccess
-		connectionCount int
-		err             error
-	}{
-		{
-			name: "single host with credentials",
-			credentials: map[string]postgres.Credentials{
-				"localhost:5432": {
-					Name:     "iam_creator",
-					Password: "",
-				},
-			},
-			hostAccess: grants.HostAccess{
-				"localhost:5432/postgres": []grants.ReadWriteAccess{},
-			},
-			connectionCount: 1,
-			err:             nil,
-		},
-		{
-			name: "multiple hosts without upstream",
-			credentials: map[string]postgres.Credentials{
-				"localhost:5432": {
-					Name:     "iam_creator",
-					Password: "",
-				},
-				"unknown": {
-					Name:     "iam_creator",
-					Password: "12345678",
-				},
-			},
-			hostAccess: grants.HostAccess{
-				"localhost:5432/postgres": []grants.ReadWriteAccess{},
-				"unknown/postgres":        []grants.ReadWriteAccess{},
-			},
-			connectionCount: 1,
-			err:             fmt.Errorf("connect to postgresql://iam_creator:********@unknown/postgres?sslmode=disable: dial tcp:"),
-		},
-		{
-			name:        "missing credentials",
-			credentials: map[string]postgres.Credentials{},
-			hostAccess: grants.HostAccess{
-				"localhost:5432/postgres": []grants.ReadWriteAccess{},
-			},
-			connectionCount: 0,
-			err:             fmt.Errorf("no credentials for host 'localhost:5432'"),
-		},
-	}
-	for _, tc := range tt {
-		t.Run(tc.name, func(t *testing.T) {
-			_ = test.SetLogger(t)
-
-			r := ReconcilePostgreSQLUser{
-				hostCredentials: tc.credentials,
-			}
-
-			// act
-			connections, err := r.connectToHosts(tc.hostAccess)
-
-			defer func() {
-				for _, db := range connections {
-					db.Close()
-				}
-			}()
-
-			// assert
-			t.Logf("Connections: %v", connections)
-			if tc.err != nil {
-				if !assert.Error(t, err, "an output error was expected") {
-					return
-				}
-				assert.Contains(t, err.Error(), tc.err.Error(), "error not as expected")
-			} else {
-				assert.NoError(t, err, "unexpected output error")
-			}
-			assert.Len(t, connections, tc.connectionCount, "connection count not as expected")
-		})
-	}
-}
 
 func TestParseHostCredentials(t *testing.T) {
 	tt := []struct {
@@ -179,7 +95,7 @@ func TestParseHostCredentials(t *testing.T) {
 func TestReconcile_badConfigmapReference(t *testing.T) {
 	// Set the logger to development mode for verbose logs.
 	logf.SetLogger(logf.ZapLogger(true))
-
+	logger := logf.Log
 	host := test.Integration(t)
 	var (
 		namespace     = "default"
@@ -270,13 +186,15 @@ func TestReconcile_badConfigmapReference(t *testing.T) {
 	// with database interaction
 	r := &ReconcilePostgreSQLUser{
 		client: cl,
-		hostCredentials: map[string]postgres.Credentials{
-			host: {
-				Name:     "iam_creator",
-				Password: "",
-			},
-		},
 		granter: grants.Granter{
+			Now: time.Now,
+			HostCredentials: map[string]postgres.Credentials{
+				host: {
+					Name:     "iam_creator",
+					Password: "",
+				},
+			},
+			Log:                      logger,
 			AllDatabasesReadEnabled:  true,
 			AllDatabasesWriteEnabled: true,
 			AllDatabases: func(namespace string) ([]lunarwayv1alpha1.PostgreSQLDatabase, error) {

pkg/grants/grants.go
@@ -3,6 +3,7 @@ package grants
 import (
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/go-logr/logr"
 	lunarwayv1alpha1 "go.lunarway.com/postgresql-controller/pkg/apis/lunarway/v1alpha1"
@@ -15,7 +16,13 @@ type Granter struct {
 	AllDatabasesReadEnabled  bool
 	AllDatabasesWriteEnabled bool
 	AllDatabases             func(namespace string) ([]lunarwayv1alpha1.PostgreSQLDatabase, error)
+	AllUsers                 func(namespace string) ([]lunarwayv1alpha1.PostgreSQLUser, error)
 	ResourceResolver         func(resource lunarwayv1alpha1.ResourceVar, namespace string) (string, error)
+
+	Log             logr.Logger
+	StaticRoles     []string
+	HostCredentials map[string]postgres.Credentials
+	Now             func() time.Time
 }
 
 // HostAccess represents a map of read and write access requests on host names
@@ -28,18 +35,18 @@ type ReadWriteAccess struct {
 	Access   lunarwayv1alpha1.AccessSpec
 }
 
-func (g *Granter) GroupAccesses(reqLogger logr.Logger, namespace string, reads []lunarwayv1alpha1.AccessSpec, writes []lunarwayv1alpha1.AccessSpec) (HostAccess, error) {
+func (g *Granter) groupAccesses(namespace string, reads []lunarwayv1alpha1.AccessSpec, writes []lunarwayv1alpha1.AccessSpec) (HostAccess, error) {
 	if len(reads) == 0 {
 		return nil, nil
 	}
 	hosts := make(HostAccess)
 	var errs error
 
-	err := g.groupByHosts(reqLogger, hosts, namespace, reads, postgres.PrivilegeRead, g.AllDatabasesReadEnabled)
+	err := g.groupByHosts(hosts, namespace, reads, postgres.PrivilegeRead, g.AllDatabasesReadEnabled)
 	if err != nil {
 		errs = multierr.Append(errs, err)
 	}
-	err = g.groupByHosts(reqLogger, hosts, namespace, writes, postgres.PrivilegeWrite, g.AllDatabasesWriteEnabled)
+	err = g.groupByHosts(hosts, namespace, writes, postgres.PrivilegeWrite, g.AllDatabasesWriteEnabled)
 	if err != nil {
 		errs = multierr.Append(errs, err)
 	}
@@ -50,11 +57,20 @@ func (g *Granter) GroupAccesses(reqLogger logr.Logger, namespace string, reads [
 	return hosts, errs
 }
 
-func (g *Granter) groupByHosts(reqLogger logr.Logger, hosts HostAccess, namespace string, accesses []lunarwayv1alpha1.AccessSpec, privilege postgres.Privilege, allDatabasesEnabled bool) error {
-	reqLogger = reqLogger.WithValues("privilege", privilege)
+func (g *Granter) groupByHosts(hosts HostAccess, namespace string, accesses []lunarwayv1alpha1.AccessSpec, privilege postgres.Privilege, allDatabasesEnabled bool) error {
 	var errs error
 	for i, access := range accesses {
-		reqLogger = reqLogger.WithValues("spec", access)
+		reqLogger := g.Log.WithValues("spec", access, "privilege", privilege)
+		// access it not requested to be granted yet
+		if g.Now().Before(access.Start.Time) {
+			reqLogger.Info("Skipping access spec: start time is in the future")
+			continue
+		}
+		// access request has expired
+		if !access.Stop.IsZero() && g.Now().After(access.Stop.Time) {
+			reqLogger.Info("Skipping access spec: stop time is in the past")
+			continue
+		}
 		host, err := g.ResourceResolver(access.Host, namespace)
 		if err != nil {
 			errs = multierr.Append(errs, fmt.Errorf("resolve host: %w", &AccessError{

pkg/postgres/postgres.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"strings"
 
@@ -82,9 +83,6 @@ func (p Privilege) String() string {
 func Role(log logr.Logger, db *sql.DB, name string, roles []string, databases []DatabaseSchema) error {
 	log.Info(fmt.Sprintf("Creating role %s", name))
 	query := fmt.Sprintf("CREATE ROLE %s WITH LOGIN", name)
-	if len(roles) != 0 {
-		query += fmt.Sprintf(" IN ROLE %s", strings.Join(roles, ", "))
-	}
 	_, err := db.Exec(query)
 	if err != nil {
 		pqError, ok := err.(*pq.Error)
@@ -95,22 +93,44 @@ func Role(log logr.Logger, db *sql.DB, name string, roles []string, databases []
 	} else {
 		log.Info(fmt.Sprintf("Role %s created", name))
 	}
-	if len(roles) != 0 {
-		_, err = db.Exec(fmt.Sprintf("GRANT %s TO %s", strings.Join(roles, ", "), name))
+
+	// grant database access roles to created role
+	existingRoles, err := persistedRoles(db, name)
+	if err != nil {
+		return fmt.Errorf("get existing roles: %w", err)
+	}
+	grantableRoles, revokeableRoles := rolesDiff(log, existingRoles, roles, databases)
+	log.Info(fmt.Sprintf("Found %d grantable and %d revokable roles for %s", len(grantableRoles), len(revokeableRoles), name), "grantable", grantableRoles, "revokeable", revokeableRoles)
+	if len(grantableRoles) != 0 {
+		joinedRoles := strings.Join(grantableRoles, ",")
+		_, err = db.Exec(fmt.Sprintf("GRANT %s TO %s", joinedRoles, name))
 		if err != nil {
-			return fmt.Errorf("grant static roles '%v': %w", roles, err)
+			return fmt.Errorf("grant access privileges '%s' to '%s': %w", joinedRoles, name, err)
 		}
 	}
+	if len(revokeableRoles) != 0 {
+		joinedRoles := strings.Join(revokeableRoles, ",")
+		_, err = db.Exec(fmt.Sprintf("REVOKE %s FROM %s", joinedRoles, name))
+		if err != nil {
+			return fmt.Errorf("revoke access privileges '%s' to '%s': %w", joinedRoles, name, err)
+		}
+	}
+	return nil
+}
 
+// rolesDiff returns roles to add and remove from existingRoles slice based of
+// the databases that are requested access to.
+func rolesDiff(log logr.Logger, existingRoles []string, expectedRoles []string, databases []DatabaseSchema) ([]string, []string) {
+	// append to expectedRoles for each database access request
 	for _, database := range databases {
 		var schemaPrivileges string
-		if database.Privileges == PrivilegeRead {
+		switch database.Privileges {
+		case PrivilegeRead:
 			schemaPrivileges = "read"
-		}
-		if database.Privileges == PrivilegeWrite {
+		case PrivilegeWrite:
 			schemaPrivileges = "readwrite"
-		}
-		if len(schemaPrivileges) == 0 {
+		default:
+			log.Error(errors.New("priviledge unknown"), fmt.Sprintf("dropped database '%s.%s' as priviledge '%s' (%[3]d) is invalid", database.Name, database.Schema, database.Privileges), "database", database)
 			continue
 		}
 		schema := database.Schema
@@ -118,11 +138,63 @@ func Role(log logr.Logger, db *sql.DB, name string, roles []string, databases []
 			schema = database.Name
 		}
 		schemaPrivileges = fmt.Sprintf("%s_%s", schema, schemaPrivileges)
-		log.Info(fmt.Sprintf("Granting %s to %s", schemaPrivileges, name))
-		_, err = db.Exec(fmt.Sprintf("GRANT %s TO %s", schemaPrivileges, name))
-		if err != nil {
-			return fmt.Errorf("grant access privileges '%s' on schema '%s': %w", schemaPrivileges, schema, err)
+		expectedRoles = append(expectedRoles, schemaPrivileges)
+	}
+
+	// find roles that are expected but not on the existing roles list
+	var addableRoles []string
+	for _, expectedRole := range expectedRoles {
+		if contains(existingRoles, expectedRole) {
+			continue
+		}
+		addableRoles = append(addableRoles, expectedRole)
+	}
+
+	// find existing roles that are not in the expected list
+	var removeableRoles []string
+	for _, existingRole := range existingRoles {
+		if contains(expectedRoles, existingRole) {
+			continue
+		}
+		// only remove roles that look like some we control, ie. suffixed with _read
+		// or _readwrite. This is to make sure we do not change roles granted out of
+		// band to specific users.
+		if !strings.HasSuffix(existingRole, fmt.Sprintf("_%s", PrivilegeRead.String())) && !strings.HasSuffix(existingRole, fmt.Sprintf("_%s", PrivilegeWrite.String())) {
+			continue
+		}
+		removeableRoles = append(removeableRoles, existingRole)
+	}
+
+	return addableRoles, removeableRoles
+}
+
+func contains(roles []string, role string) bool {
+	for _, r := range roles {
+		if r == role {
+			return true
 		}
 	}
-	return nil
+	return false
+}
+
+func persistedRoles(db *sql.DB, name string) ([]string, error) {
+	rows, err := db.Query("SELECT rolname FROM pg_user JOIN pg_auth_members ON (pg_user.usesysid=pg_auth_members.member) JOIN pg_roles ON (pg_roles.oid=pg_auth_members.roleid) WHERE pg_user.usename=$1", name)
+	if err != nil {
+		return nil, fmt.Errorf("select roles: %w", err)
+	}
+	defer rows.Close()
+	var roles []string
+	for rows.Next() {
+		var rolName string
+		err = rows.Scan(&rolName)
+		if err != nil {
+			return nil, fmt.Errorf("scan row: %w", err)
+		}
+		roles = append(roles, rolName)
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil, fmt.Errorf("scanning rows: %w", err)
+	}
+	return roles, nil
 }

commit 5f2e7c39d191678e914de28defa6d6d7a2357e3f

Respect role prefix
Roles are not created with the configured role-prefix. The bug was introduced in
46e333a80e8dd6ea7829ccd53c3d9578ef0c0575 (#54) where the user reconciliation
logic was moved out of package postgresqluser.

This change set prefixes the PostgreSQLUser specifications name with the
configured prefix in the controller.

pkg/controller/postgresqluser/postgresqluser_controller.go
@@ -212,7 +212,11 @@ func (r *ReconcilePostgreSQLUser) reconcile(reqLogger logr.Logger, request recon
 	}
 
 	// User instance created or updated
-	reqLogger = reqLogger.WithValues("user", user.Spec.Name)
+
+	// prefix name with configured rolePrefix
+	user.Spec.Name = fmt.Sprintf("%s%s", r.rolePrefix, user.Spec.Name)
+
+	reqLogger = reqLogger.WithValues("user", user.Spec.Name, "rolePrefix", r.rolePrefix)
 	reqLogger.Info("Reconciling found PostgreSQLUser resource", "user", user.Spec.Name)
 
 	err = r.granter.SyncUser(reqLogger, request.Namespace, *user)

pkg/grants/sync.go
@@ -15,6 +15,7 @@ import (
 // defined in the host instances. Any excessive roles are removed and missing
 // ones are added.
 func (g *Granter) SyncUser(log logr.Logger, namespace string, user lunarwayv1alpha1.PostgreSQLUser) error {
+	log.Info(fmt.Sprintf("Syncing user %s", user.Spec.Name), "user", user)
 	//   resolve required grants taking expiration into account
 	//   diff against existing
 	//   revoke/grant what is needed
@@ -38,7 +39,7 @@ func (g *Granter) SyncUser(log logr.Logger, namespace string, user lunarwayv1alp
 		}
 	}()
 
-	err = g.setRolesOnHosts(log, user.Name, accesses, hosts)
+	err = g.setRolesOnHosts(log, user.Spec.Name, accesses, hosts)
 	if err != nil {
 		return fmt.Errorf("grant access on host: %w", err)
 	}

pkg/controller/postgresqluser/postgresqluser_controller_test.go
@@ -2,6 +2,7 @@ package postgresqluser
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 	"time"
 
@@ -241,4 +242,148 @@ func TestReconcile_badConfigmapReference(t *testing.T) {
 		Requeue:      false,
 		RequeueAfter: 0,
 	}, res, "result not as expected")
+}
+
+// TestReconcile_rolePrefix tests that reconciliations respect the rolePrefix
+// setting. The PostgreSQLUser reconciler is configured with a prefix and a
+// database and user are reconciled. Then a connect attempt is done with the
+// prefixed user name.
+func TestReconcile_rolePrefix(t *testing.T) {
+	// Set the logger to development mode for verbose logs.
+	logf.SetLogger(logf.ZapLogger(true))
+
+	host := test.Integration(t)
+	var (
+		namespace     = "default"
+		database1Name = "database1"
+		userName      = "user"
+		rolePrefix    = "iam_developer_"
+
+		// user requesting access to all databases on host
+		userResource = &lunarwayv1alpha1.PostgreSQLUser{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      userName,
+				Namespace: namespace,
+			},
+			Spec: lunarwayv1alpha1.PostgreSQLUserSpec{
+				Name: userName,
+				Read: []lunarwayv1alpha1.AccessSpec{
+					{
+						Host: lunarwayv1alpha1.ResourceVar{
+							Value: host,
+						},
+						AllDatabases: true,
+					},
+				},
+			},
+		}
+
+		// valid database on host
+		database1Resource = &lunarwayv1alpha1.PostgreSQLDatabase{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      database1Name,
+				Namespace: namespace,
+			},
+			Spec: lunarwayv1alpha1.PostgreSQLDatabaseSpec{
+				Name: database1Name,
+				Host: lunarwayv1alpha1.ResourceVar{
+					Value: host,
+				},
+				Password: lunarwayv1alpha1.ResourceVar{
+					Value: "123456",
+				},
+				User: lunarwayv1alpha1.ResourceVar{
+					Value: database1Name,
+				},
+			},
+		}
+	)
+
+	// Register operator types with the runtime scheme.
+	s := scheme.Scheme
+	s.AddKnownTypes(lunarwayv1alpha1.SchemeGroupVersion, database1Resource)
+	s.AddKnownTypes(lunarwayv1alpha1.SchemeGroupVersion, userResource)
+	s.AddKnownTypes(lunarwayv1alpha1.SchemeGroupVersion, &lunarwayv1alpha1.PostgreSQLDatabaseList{})
+
+	// Add tracked objects to the fake client simulating their existence in a k8s
+	// cluster
+	objs := []runtime.Object{
+		database1Resource,
+		userResource,
+	}
+	cl := fake.NewFakeClient(objs...)
+
+	// Create a controller object with the fake client but otherwise "live" setup
+	// with database interaction
+	r := &ReconcilePostgreSQLUser{
+		client:     cl,
+		rolePrefix: rolePrefix,
+		granter: grants.Granter{
+			Now: time.Now,
+			HostCredentials: map[string]postgres.Credentials{
+				host: {
+					Name:     "iam_creator",
+					Password: "",
+				},
+			},
+			AllDatabasesReadEnabled:  true,
+			AllDatabasesWriteEnabled: true,
+			AllDatabases: func(namespace string) ([]lunarwayv1alpha1.PostgreSQLDatabase, error) {
+				return kube.PostgreSQLDatabases(cl, namespace)
+			},
+			ResourceResolver: func(resource lunarwayv1alpha1.ResourceVar, namespace string) (string, error) {
+				return kube.ResourceValue(cl, resource, namespace)
+			},
+		},
+		setAWSPolicy: func(log logr.Logger, credentials *credentials.Credentials, policy iam.AWSPolicy, userID string) error {
+			return nil
+		},
+	}
+
+	// seed database1 into the postgres host
+	dbConn, err := postgres.Connect(logf.Log, postgres.ConnectionString{
+		Database: "postgres",
+		Host:     host,
+		Password: "",
+		User:     "iam_creator",
+	})
+	if !assert.NoError(t, err, "failed to connect to database to seed database") {
+		return
+	}
+	err = postgres.Database(logf.Log, dbConn, host, postgres.Credentials{
+		Name:     database1Name,
+		Password: "123456",
+		User:     database1Name,
+	})
+	if !assert.NoError(t, err, "failed to seed database") {
+		return
+	}
+
+	// reconcile user requesting access to all databases with a bad database
+	// reference
+	req := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      userName,
+			Namespace: namespace,
+		},
+	}
+	res, err := r.Reconcile(req)
+	assert.NoError(t, err, "reconciliation failed")
+	assert.Equal(t, reconcile.Result{
+		Requeue:      false,
+		RequeueAfter: 0,
+	}, res, "result not as expected")
+
+	// assert that the user can connect with a prefixed role
+	userConn, err := postgres.Connect(logf.Log, postgres.ConnectionString{
+		Database: database1Name,
+		Host:     host,
+		Password: "",
+		User:     fmt.Sprintf("%s%s", rolePrefix, userName), // simulates what users will sign in with through AWS
+	})
+	if !assert.NoError(t, err, "failed to connect to database with prefixed role") {
+		return
+	}
+	_, err = userConn.Query("SELECT 1")
+	assert.NoError(t, err, "failed to query")
 }